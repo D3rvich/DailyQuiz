5d186d22f934f7ab5d882a8be4d8a0d8386182dd
D3rvich <d3rvich@gmail.com>
2025-11-09 20:00:03 +0500

    Add max width to results screen.

--- feature/quiz/src/main/java/ru/d3rvich/quiz/screens/ResultsScreen.kt
+++ feature/quiz/src/main/java/ru/d3rvich/quiz/screens/ResultsScreen.kt
@@ -3,17 +3,20 @@ package ru.d3rvich.quiz.screens
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowSizeClass
 import ru.d3rvich.quiz.R
 import ru.d3rvich.ui.components.QuizResultCard
 import ru.d3rvich.ui.theme.DailyQuizTheme
@@ -25,23 +28,28 @@ fun ResultsScreen(
     navigateToStart: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val isLandscape = !(currentWindowAdaptiveInfo().windowSizeClass
+        .isHeightAtLeastBreakpoint(WindowSizeClass.HEIGHT_DP_MEDIUM_LOWER_BOUND))
     Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            text = stringResource(R.string.results),
-            style = MaterialTheme.typography.displaySmall,
-            fontWeight = FontWeight.Bold,
-            letterSpacing = TextUnit(0f, TextUnitType.Sp),
-            modifier = Modifier.padding(40.dp)
-        )
+        if (!isLandscape) {
+            Text(
+                text = stringResource(R.string.results),
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.Bold,
+                letterSpacing = TextUnit(0f, TextUnitType.Sp),
+                modifier = Modifier.padding(40.dp)
+            )
+        }
         QuizResultCard(
             correctAnswers = correctAnswers,
             totalQuestions = totalQuestions,
-            onRetryClick = navigateToStart
+            onRetryClick = navigateToStart,
+            modifier = Modifier.widthIn(max = 600.dp)
         )
     }
 }
 
-@Preview(showBackground = true, apiLevel = 35)
+@PreviewScreenSizes
 @Composable
 private fun ResultsPreview() {
     DailyQuizTheme {
