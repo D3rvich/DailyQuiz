2c8018213c843d3a6e07187be55ee4fc3e60bd85
D3rvich <d3rvich@gmail.com>
2025-12-10 18:51:00 +0500

    Add RoomConventionPlugin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,23 +1,14 @@
-import com.google.devtools.ksp.gradle.KspExtension
-
 plugins {
     alias(libs.plugins.dailyquiz.android.library)
     alias(libs.plugins.dailyquiz.android.hilt)
-    alias(libs.plugins.room)
-    alias(libs.plugins.ksp)
+    alias(libs.plugins.dailyquiz.android.room)
     alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "ru.d3rvich.database"
-    room {
-        schemaDirectory("$projectDir/schemas")
-    }
     sourceSets {
         getByName("androidTest").assets.srcDir("$projectDir/schemas")
-    }
-    extensions.configure<KspExtension> {
-        arg("room.generateKotlin", "true")
     }
     defaultConfig {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -26,9 +17,6 @@ android {
 
 dependencies {
     implementation(project(":core:domain"))
-    implementation(libs.androidx.room.runtime)
-    ksp(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
     implementation(libs.kotlinx.serializationJson)
     implementation(libs.kotlinx.datetime)
     testImplementation(libs.junit)
